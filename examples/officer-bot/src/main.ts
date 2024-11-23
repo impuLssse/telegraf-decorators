8a98506dcae50206c38be9de189ff0af1305e3e0
Maxim Safronov <carpet.chores0v@icloud.com>
2024-11-23 16:38:08 +0300

    Added line in npmignore

---
+++ examples/officer-bot/src/main.ts
@@ -0,0 +1,49 @@
+import path from "path";
+
+/** Подгружаем конфиг с помощью dotenv */
+import { config } from "dotenv";
+config({ path: path.resolve(__dirname, "../.env") });
+const appConfig = process.env as IConfig;
+
+import knex from "knex";
+import { session, Telegraf } from "telegraf";
+import { RootModule } from "telegraf-ecosystem";
+import { IConfig, IContext, SceneContract } from "./shared.types";
+
+export const knexClient = knex({
+  client: "pg",
+  connection: appConfig.DATABASE_URL,
+});
+
+import "./scenes";
+import { Stage } from "telegraf/typings/scenes";
+
+export async function bootstrapBot(): Promise<void> {
+  const bot = new Telegraf<IContext>(appConfig.BOT_TOKEN);
+
+  /** Используем middleware для работы с сессиями */
+  bot.use(session());
+
+  const stage = new Stage<IContext>();
+  const rootModule = new RootModule(bot, stage);
+  // rootModule.registerUpdates([...RootModule.updatesRegistry]);
+  rootModule.registerScenes([...RootModule.scenesRegistry], {
+    onSceneRegistered(sceneId) {
+      console.log(`Scene registered: ${sceneId}`);
+    },
+  });
+
+  bot.start(async (ctx) => {
+    console.log(999);
+    console.log(ctx);
+    // await ctx.scene.enter(SceneContract.Home);
+  });
+
+  /**
+   * Запуск бота.
+   * Нельзя запускать асинхронно, потому что под капотом бесконечный асинхронный итератор
+   */
+  bot.launch();
+  console.log("Bot is running...");
+}
+bootstrapBot();
